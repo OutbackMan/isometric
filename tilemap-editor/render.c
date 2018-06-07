32b361ec9979f93239b4df0c11965f54d09a5e7c
Ryan <re.mcclue@gmail.com>
2018-06-07 15:38:12 +1000

    asdsa

--- tilemap-editor/render.c
+++ tilemap-editor/render.c
@@ -19,6 +19,7 @@ static void render_editor_background(SDL_Renderer* renderer)
 	_SDL_RenderFillRect(renderer, NULL);
 }
 
+// spacing_x may be equivalent to tile render width
 static void render_editor_tilemap(SDL_Renderer* renderer)
 {
 	// START(background)
@@ -45,7 +46,7 @@ static void render_editor_tilemap(SDL_Renderer* renderer)
 		for (int tile_y = visible_tile_y; tile_count < num_visible_tiles_y; ++tile_count) {
 			int render_x = tile_x * tile_map->spacing_x - tile_map->input_values->scroll_x;
 			int render_y = tile_y * tile_map->spacing_y - tile_map->input_values->scroll_y;
-			render_tile(tile_map, tile_x, tile_y, render_x, render_y);
+			render_tile(tile_map, tile_x, tile_y, render_x, render_y); // tile_paint
 		}
 	}
 	// END(tiles)	
@@ -62,14 +63,11 @@ static void render_editor_tilemap(SDL_Renderer* renderer)
 	// END(grid)
 	
 	// START(more_tiles)
-	more_tile(); // stbte_tile
+	tile(); // stbte_tile
+		tile_outline()
+		handle_event_based_on_active_tool()
 	// END(more_tiles)
 
-}
-
-static void more_tile()
-{
-	
 }
 
 static void render_tile(SDL_Renderer* renderer, int tile_x, int tile_y, int render_x, int render_y)
@@ -79,16 +77,44 @@ static void render_tile(SDL_Renderer* renderer, int tile_x, int tile_y, int rend
 	int x1 = x0 + tile_map->spacing_x;
 	int y1 = y0 + tile_map->spacing_y;
 
-	if (MAP_IS_HOT) {
-		if (editor->is_pasting) {
-			int ox = tile_x - editor->paste_x;		
-			int oy = tile_y - editor->paste_y;
-			if (ox >= 0 && ox < editor->copy_width && oy >= 0 && oy < editor->copy_height) {
-				paste_stack();		
-			}
-		}		
+	bool mouse_is_over_tile = mouse_hittest(x0, y0, x1, y1);
+
+	if (editor->input_values->is_dragging) {
+		int ox = tile_x - editor->input_values->drag_x		
+		int oy = tile_y - editor->input_values->drag_y		
+		// remove underlying tiles, unless holding shift
+		if (!editor->input_values->is_shift_pressed && ox >= 0 && ox <= editor->input_values->drag_w) {
+			clear_stack();
+		}
+		int ox = tile_x - editor->input_values->drag_dest_x;
+		int oy = tile_y - editor->input_values->drag_dest_y;
+		paste_stack();
+	} else if (tile_map->is_active) {
+		if (editor->active_event == SDL_MOUSEDOWN_LEFT) {
+			brush_predict(); // determine what layer we can use the brush on
+		} else {
+			erase_predict();		
+		}
 	}
 }
+
+// hot means in the process of handling an event. 
+// active means it is ready to handle an event.
+static void tile(int sx, int sy, int map_x, int map_y)
+{
+	int x0 = render_x;
+	int y0 = render_y;
+	int x1 = x0 + tile_map->spacing_x;
+	int y1 = y0 + tile_map->spacing_y;
+
+	bool mouse_is_over_tile = mouse_hittest(x0, y0, x1, y1);
+
+	switch (editor->active_event) {
+		
+	}
+	
+}
+
 
 void draw_tilemap()
 {
@@ -133,4 +159,16 @@ static void compute_panel_locations(Editor* editor)
 	min_width[PANEL_INFO]
 	min_width[PANEL_TILES]
 	min_width[PANEL_TOOLBAR]
+}
+
+static void draw_panel_bodies()
+{
+	for (size_t panel_index = 0; panel_index < NUM_PANELS; ++panel_index) {
+		switch (panel_index) {
+		case PANEL_TOOLBAR:		
+			render_panel_toolbar();
+			__SDL_RenderRect(x0, y0, x1, y1, COLOR_GROUP_PANEL, COLOUR_VARIANT_IDLE);		
+		}
+	}		
+	render_panel_side_selectors()
 }
