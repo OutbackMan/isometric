71e5a7012ca7cbc2f5b8d373b5ec95a3d9a32aee
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-06-15 10:08:09 +1000

    Create multithreading.c

---
+++ multithreading.c
@@ -0,0 +1,17 @@
+static int thread_function(void* data)
+{
+  while (global_is_running) {
+    char* msg = (char *)data;
+    puts(msg);
+  } // as compiler doesn't know about threads, may have to use 'volatile'
+  
+  return 0;
+}
+
+int game_main(void)
+{
+  char* msg = "hello from the thread";
+  SDL_Thread* thread = SDL_CreateThread(thread_function, "thread_name", msg);
+
+  return 0;
+}
