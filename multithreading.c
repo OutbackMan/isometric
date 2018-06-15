193447fd9a21032b2d312f22dacb0c56c9703ad1
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-06-15 13:02:12 +1000

    Update multithreading.c

--- multithreading.c
+++ multithreading.c
@@ -13,21 +13,26 @@ typedef struct {
   char* string_to_print;
 } WorkQueueEntry;
 
+GLOBAL uint32_t volatile entry_completion_count = 0;
 GLOBAL uint32_t volatile next_entry_to_do = 0;
 GLOBAL uint32_t volatile entry_count = 0;
 WorkQueueEntry entries[256];
 
-static void push_string(const char* str)
+static void push_string(SDL_Sem* semaphore_handle, const char* str)
 {
+  SDL_assert(entry_count < array_count(entries));
   // threads may read data before it is written (compiler may move the increment before for optimisation)
   entries[entry_count].string_to_print = str;
   
   COMPLETE_PAST_WRITES_BEFORE_FUTURE_WRITES;
   
   ++entry_count;
+  
+  SDL_SemPost(semaphore_handle);
 }
 
 typedef struct {
+  SDL_sem* semaphore_handle;
   int logical_thread_index;
 } TheadInfo;
 
@@ -37,10 +42,13 @@ INTERNAL int thread_function(void* thread_param)
  
   while (true) {
     if (next_entry_to_do < entry_count) {
-      int entry_index = next_entry_to_do++; // not interlocked, not volatile
-      
-      // these reads are not in order
+      int entry_index = InterlockedIncrement(&next_entry_to_do) - 1; // just use SDL_Lock()
+      COMPLETE_PAST_READS_BEFORE_FUTURE_READS;
       puts(entries[entry_index]);
+      
+      InterlockedIncrement(&entry_completion_count);
+    } else {
+      SDL_SemWait(thread_info->semaphore_handle);
     }
   }
   
@@ -50,16 +58,22 @@ INTERNAL int thread_function(void* thread_param)
 int game_main(void)
 {
   ThreadInfo thread_info[4];
-  for (int thread_index = 0; thread_index < 4; ++thread_index) {
-    thread_info[thread_index] = { .logical_thread_index = thread_index };
+  
+  uint32_t starting_active_threads = 0;
+  SDL_sem* semaphore_handle = SDL_CreateSemaphore(starting_active_threads);
+  
+  for (int thread_index = 0; thread_index < ARRAY_COUNT(thread_info); ++thread_index) {
+    thread_info[thread_index] = { .logical_thread_index = thread_index, .semaphore_handle = semaphore_handle };
     SDL_Thread* thread = SDL_CreateThread(thread_function, NULL, &thread_info);
   }
   
-  push_string("string 0");
-  push_string("string 1");
-  push_string("string 2");
-  push_string("string 3");
-  push_string("string 4");
+  push_string(semaphore_handle, "string 0");
+  push_string(semaphore_handle, "string 1");
+  push_string(semaphore_handle, "string 2");
+  push_string(semaphore_handle, "string 3");
+  push_string(semaphore_handle, "string 4");
+  
+  while (entry_count != entry_completion_count); // wait till all work has been done
 
   return 0;
 }
