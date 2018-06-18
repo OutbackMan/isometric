ca496b3e27de6f28188004df15994e0a7ed5fdd9
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-06-18 17:14:34 +1000

    Update asset-manager.h

--- asset-manager.h
+++ asset-manager.h
@@ -1,44 +1,47 @@
 // images: png
 // music: ogg vorbis 44.1kHz
 // sound effects: wav
-
+/*
+int x =
+	10;
+*/
 // load assets via threads
+
 typedef enum {
-	ASSET_TEXTURE_GRASS,
-	ASSET_TEXTURE_SWORD,
-	
-	ASSET_TEXTURE___NUM
+	TEXTURE_GRASS,
+	TEXTURE_STONE,
+	TEXTURE_WOOD,
+	TEXTURE__NUM
 } ASSET_TEXTURE_ID;
 
-inline SDL_Texture* get_texture(GameState* game_state, ASSET_ID id)
-{
-	return game_state->assets[id];	
-}
-
-inline void draw_texture(GameState, ASSET_ID id)
-{
-	if (id < 0 || id >= ASSET__NUM) {
-		
-	}
-	SDL_RenderCopyEx();	
-}
-
 typedef struct {
-	SDL_Texture *textures[ASSET_NUM]; // as accessing memory, should use getters and setters
+	SDL_Texture *textures[TEXTURE__NUM];
 	TTF_Font *fonts[];
 	MIX_Music *music[];
 } Assets;
 
-void load_textures()
+inline SDL_Texture* assets_texture_get(Assets* assets, ASSET_TEXTURE_ID texture_id)
+{
+	return assets->textures[texture_id];
+}
 
-typedef struct {
+void assets_texture_render(Assets* assets, ASSET_TEXTURE_ID texture_id)
+{
+	if (assets_texture_get(assets, texture_id) != NULL) {
+		SDL_RenderCopyEx();
+	}
+	
+}
 
-} GameState;
+void assets_texture_load(Assets* assets)
+{
+	assets->textures[TEXTURE_SWORD] = SDL_LoadText("sword.png");
+}
 
 
 int main(void)
 {
-	game_state->assets[ASSET_GRASS] = load_texture("grass.bmp");	
+	game_state->assets[ASSET_TEXTURE_GRASS] = load_texture(ASSET_TEXTURE_GRASS);	
 
 	return 0;	
 }
