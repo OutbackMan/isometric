973a407788c5ec2d80d8858baea4c2473d7086c7
Ryan <re.mcclue@gmail.com>
2018-06-18 14:19:21 +1000

    sdas

--- asset-manager.h
+++ asset-manager.h
@@ -2,15 +2,43 @@
 // music: ogg vorbis 44.1kHz
 // sound effects: wav
 
-typedef struct {
-	Map* texture_map; // music, fonts, etc.
-} AssetManager;
+// load assets via threads
+typedef enum {
+	ASSET_TEXTURE_GRASS,
+	ASSET_TEXTURE_SWORD,
+	
+	ASSET_TEXTURE___NUM
+} ASSET_TEXTURE_ID;
 
-create
-
-void asset_manager_set_texture(const char* key_name, const char* texture_location)
+inline SDL_Texture* get_texture(GameState* game_state, ASSET_ID id)
 {
-	SDL_Texture* texture = i_common_load_texture(texture_location);
+	return game_state->assets[id];	
+}
 
-	asset_manager->map(key_name, texture);
+inline void draw_texture(GameState, ASSET_ID id)
+{
+	if (id < 0 || id >= ASSET__NUM) {
+		
+	}
+	SDL_RenderCopyEx();	
+}
+
+typedef struct {
+	SDL_Texture *textures[ASSET_NUM]; // as accessing memory, should use getters and setters
+	TTF_Font *fonts[];
+	MIX_Music *music[];
+} Assets;
+
+void load_textures()
+
+typedef struct {
+
+} GameState;
+
+
+int main(void)
+{
+	game_state->assets[ASSET_GRASS] = load_texture("grass.bmp");	
+
+	return 0;	
 }
